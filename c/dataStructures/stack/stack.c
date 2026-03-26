498a32eceae9bb30f7ebd83bc5bf1fa16a12a981
Riyaz-SDE <riyazudeen574@gmail.com>
2026-03-26 23:20:31 +0530

    riyaz-Commit

---
+++ c/dataStructures/stack/stack.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// implement structure
+// funtion like push pop size
+#define MAX_SIZE 5
+typedef struct
+{
+    int items[MAX_SIZE];
+    int top
+} stack;
+
+void intStact(stack *s){
+    s -> top = -1;
+}
+int isEmpty(stack *s){
+    if(s->top == -1) return 0
+}
+int main(){
+
+}
